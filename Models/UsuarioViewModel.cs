43aac7464e51c2586767cb9b03b622b5581ee6ac
Luiz <lzsouza987@hotmail.com>
2021-11-08 00:26:01 -0300

    primeiro commit

---
+++ Models/UsuarioViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace RpgMvc.Models
+{
+    public class UsuarioViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordString { get; set; }        
+        public byte[] Foto { get; set; }
+    }
+}
+
+
+
